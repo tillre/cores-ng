ee6eda027a1fd1c65f8c378d166c50eea066e71f
Till Reitemeyer <till@skoni.de>
2013-06-13 16:42:16 +0200

    use concatenated js for tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,10 +19,12 @@ files = [
   '../node_modules/chai/chai.js',
 
   // order matters
-  '../lib/index.js',
-  '../lib/templates.js',
-  '../lib/services/**/*.js',
-  '../lib/directives/**/*.js',
+  // '../lib/index.js',
+  // '../lib/templates.js',
+  // '../lib/services/**/*.js',
+  // '../lib/directives/**/*.js',
+
+  '../cores.js',
   
   './unit/test.js'
 ];
