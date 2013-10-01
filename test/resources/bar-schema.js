9a2ed4e27e239dac5aa118fb20cc720d6c77393a
Till Reitemeyer <till@skoni.de>
2013-10-01 13:18:23 +0200

    add simple markdown directive with preview mode, some style changes

--- test/resources/bar-schema.js
+++ test/resources/bar-schema.js
@@ -8,57 +8,56 @@ module.exports = j.object({
   string1: j.string().minLength(2).maxLength(10).pattern('[a-zA-Z]+'),
   string2: j.string(),
 
-  richRext: j.string().custom('view', 'cr-rich-text')
+  markdown: j.string().custom('view', 'cr-rich-text'),
 
-//   slug: j.string()
-//     .format('slug')
-//     .custom('view', { type: 'cr-slug', source: ['string1', 'string2'] }),
+  slug: j.string()
+    .format('slug')
+    .custom('view', { type: 'cr-slug', source: ['string1', 'string2'] }),
 
-//   date: j.string().custom('view', 'cr-datetime'),
+  date: j.string().custom('view', 'cr-datetime'),
 
-//   'enum': j.enum(1, 2, 3),
+  'enum': j.enum(1, 2, 3),
 
-//   ref: j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': '/bar'}),
+  ref: j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': '/bar'}),
 
-//   singleSelRef: j.ref('Foo').custom('view', { type: 'cr-single-select-ref', 'preview-path': '/bar' }),
+  singleSelRef: j.ref('Foo').custom('view', { type: 'cr-single-select-ref', 'preview-path': '/bar' }),
 
-//   multiSelRef: j.array(j.ref('Foo')).custom('view', { type: 'cr-multi-select-ref', 'preview-path': '/bar' }),
+  multiSelRef: j.array(j.ref('Foo')).custom('view', { type: 'cr-multi-select-ref', 'preview-path': '/bar' }),
 
-//   array: j.array(j.object({
-//     foo: j.boolean()
-//   })),
+  array: j.array(j.object({
+    foo: j.boolean()
+  })),
 
-//   arrayRefs: j.array(
-//     j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': 'bar' })
-//   ),
+  arrayRefs: j.array(
+    j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': 'bar' })
+  ),
 
-//   anyof: j.array(j.anyOf(
-//     j.object({
-//       text: j.string(),
-//       images: j.array(j.object({ name: j.string() }))
-//     }).custom('name', 'textimage'),
+  anyof: j.array(j.anyOf(
+    j.object({
+      text: j.string(),
+      images: j.array(j.object({ name: j.string() }))
+    }).custom('name', 'textimage'),
 
-//     j.object({
-//       embed: j.string()
-//     }).custom('name', 'video'),
+    j.object({
+      embed: j.string()
+    }).custom('name', 'video'),
 
-//     j.object({
-//       images: j.array(j.object({ name: j.string() }))
-//     }).custom('name', 'gallery')
-//   )),
+    j.object({
+      images: j.array(j.object({ name: j.string() }))
+    }).custom('name', 'gallery')
+  )),
 
-//   anyofRefs: j.array(j.anyOf(
-//     j.object({ foo: j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': 'bar' }) }).custom('name', 'foo1'),
-//     j.object({ bar: j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': 'bar' }) }).custom('name', 'foo2')
-//   )),
+  anyofRefs: j.array(j.anyOf(
+    j.object({ foo: j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': 'bar' }) }).custom('name', 'foo1'),
+    j.object({ bar: j.ref('Foo').custom('view', { type: 'cr-ref', 'preview-path': 'bar' }) }).custom('name', 'foo2')
+  )),
 
-//   object: j.object({
-//     foo: j.number(),
-//     bar: j.string()
-//   }),
+  object: j.object({
+    foo: j.number(),
+    bar: j.string()
+  }),
 
-//   text: j.string().custom('view', 'cr-text'),
-//   password: j.string().minLength(8).custom('view', 'cr-password')
+  text: j.string().custom('view', 'cr-text'),
+  password: j.string().minLength(8).custom('view', 'cr-password')
 
-// }).required('string2', 'number', 'ref', 'slug', 'text', 'singleSelRef');
-});
+}).required('string2', 'number', 'ref', 'slug', 'text', 'singleSelRef');
